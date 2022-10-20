49c84924ecb2212774689796f304739beb86fe1c
Justin Wing Chung Hui <wingchung@justinhui.co.uk>
2022-10-20 21:50:02 +0100

    Added data and revolving key word

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <GoogleImageLoader :searchTerm="searchTerm" />
 </template>
 
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import GoogleImageLoader from './components/GoogleImageLoader.vue'
+
+const data = ref(new Array<string>())
+const searchTerm = ref("Brian May")
+
+onMounted(async () => {
+  const response = await fetch('/events2020.txt');
+  const text = await response.text()
+
+  data.value = text.split('\n')
+
+  window.setInterval(setRandomSearchTerm, 3000);
+});
+
+const setRandomSearchTerm = () => {  
+  const index = Math.floor(Math.random() * data.value.length);
+  searchTerm.value = data.value[index];
+};
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/GoogleImageLoader.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ displayTerm }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+const props = defineProps<{ searchTerm: string }>()
+
+const displayTerm = ref('')
+watchEffect(() => {
+  displayTerm.value = props.searchTerm;
+});
+
+</script>
+
+<style scoped>
+</style>
